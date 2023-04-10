49629495131646122b83c91aa7e54ad446f1e231
Alberto Pellizzari <alberto.pellizzari@bft-automation.com>
2023-04-10 17:32:44 +0200

    provo compilazione in ci/cd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import './App.css';
+import Amplify from 'aws-amplify';
+import awsConfig from './aws-exports';
+import { Authenticator } from '@aws-amplify/ui-react';
+
+Amplify.configure(awsConfig);
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to Amplify</h1>
-    </div>
-  );
+    <Authenticator> 
+      <div className="App">
+        <h1>Welcome to Amplify AW</h1>
+        <signOut></signOut>
+      </div>
+    </Authenticator>
+  );  
 }
 
 export default App;
